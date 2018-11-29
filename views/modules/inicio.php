139c92c6b973a5165c1bef7105be2b8fd58b097c
raulmg <raul.mtzglez@gmail.com>
2018-11-28 22:48:06 -0600

    primer cambio inicio
    Diseño de la pagina de inicio

--- views/modules/inicio.php
+++ views/modules/inicio.php
@@ -1,15 +1,15 @@
 <?php
    session_start();
-  // if(!$_SESSION['validar']){
-  //   header("location:ingreso");
-  //   exit();
-  // }else{
-  //   include "header.php";
-  //   include "menu.php";
-  // }
+  if(!$_SESSION['validar']){
+    header("location:ingreso");
+    exit();
+  }else{
+    include "header.php";
+    include "menu.php";
+  }
 
-  include "header.php";
-  include "menu.php";
+  // include "header.php";
+  // include "menu.php";
  ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -19,8 +19,21 @@
       <div class="col-md-12">
         <div class="box">
           <div class="box-header with-border">
-            <h1 class="box-title">Tablero</h1>
-            
+            <h1 class="box-title">Título de página de inicio</h1>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-lg-offset-2 col-lg-8">
+                <h3>Descripcion de la pagina de inicio</h3>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-lg-offset-2 col-lg-8">
+                <img src="views/img/sacsi/logo_sacsi.jpg" class="img img-responsive" alt="">
+              </div>
+            </div>
+
           </div>
         </div><!-- /.box -->
       </div><!-- /.col -->
