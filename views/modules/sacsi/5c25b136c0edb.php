b5497b0da154ea45394ce6c6b046f243315cdebd
raulmg <raul.mtzglez@gmail.com>
2019-02-26 23:26:40 -0600

    Template v2
    Version2

--- views/modules/sacsi/5c25b136c0edb.php
+++ views/modules/sacsi/5c25b136c0edb.php
@@ -16,6 +16,9 @@
                 <li>Sistemas</li>
                 <li>Procedimientos</li>
                 <li class="active"><a href="#"><i class="fa fa-plus-circle"></i> Nuevo</a></li>
+                <li class="pull-right">
+
+                </li>
               </ol>
             </div>
 
@@ -30,6 +33,8 @@
                     <th class="text-center">Versión</th>
                     <th class="text-center">Tipo</th>
                     <th class="text-center">Última revisión</th>
+                    <th class="text-center">Opciones</th>
+
                   </thead>
                   <tbody>
                     <tr>
@@ -40,6 +45,21 @@
                       <td>0</td>
                       <td>Procedimiento</td>
                       <td>2019-01-25</td>
+                      <td>
+                        <div class="dropdown">
+                          <button class="btn btn-default btn-sm dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            Dropdown
+                            <span class="caret"></span>
+                          </button>
+                          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                          </ul>
+                        </div>
+                      </td>
                     </tr>
                   </tbody>
 
